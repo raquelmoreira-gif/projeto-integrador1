9448197aca250864602c360b9371f3d611e11a22
Camila S. <cnstos+git@gmail.com>
2026-04-07 12:30:37 -0300

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Front funcionando");
+
+  const btn = document.getElementById("btnAbrirCaixa");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Caixa aberto com sucesso!");
+    });
+  }
+});
