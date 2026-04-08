0b0e4f1efc045103a810d0755078b2fdfa4ab825
Raquel <raquel.moreirarg23@gmail.com>
2026-04-07 23:36:36 -0300

    Refactor imports and comments in __init__.py
    Removed unused imports and updated comments for clarity.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask
+
 from app.routes.caixa import caixa_bp
 from app.routes.health import health_bp
 from app.routes.produtos import produtos_bp
@@ -9,17 +10,14 @@ from app.routes.vendas import vendas_bp
 def create_app():
     app = Flask(__name__)
 
-    # Registrar Blueprints
+    # Registrar rotas
     app.register_blueprint(health_bp)
     app.register_blueprint(caixa_bp)
     app.register_blueprint(produtos_bp)
     app.register_blueprint(usuarios_bp)
     app.register_blueprint(vendas_bp)
     app.register_blueprint(relatorios_bp)
-
-    #Rotas raiz funcionando igual ao / health
-    @app.route("/")
-    def home():
-        return jsonify ({"sucess": True, "data": "API online"}), 200
+   
+    print(app.url_map)
 
     return app
