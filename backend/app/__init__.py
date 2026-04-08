1c7d72d2ad97fd5bfe83ad92a1e02dacf63b2a87
Raquel <raquel.moreirarg23@gmail.com>
2026-04-07 22:56:04 -0300

    Add home route for API online status
    Added a home route that returns API status.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 from app.routes.caixa import caixa_bp
 from app.routes.health import health_bp
 from app.routes.produtos import produtos_bp
@@ -7,18 +6,20 @@ from app.routes.relatorios import relatorios_bp
 from app.routes.usuarios import usuarios_bp
 from app.routes.vendas import vendas_bp
 
-
 def create_app():
     app = Flask(__name__)
 
-    # Registrar rotas
+    # Registrar Blueprints
     app.register_blueprint(health_bp)
     app.register_blueprint(caixa_bp)
     app.register_blueprint(produtos_bp)
     app.register_blueprint(usuarios_bp)
     app.register_blueprint(vendas_bp)
     app.register_blueprint(relatorios_bp)
-   
-    print(app.url_map)
+
+    #Rotas raiz funcionando igual ao / health
+    @app.route("/")
+    def home():
+        return jsonify ({"sucess": True, "data": "API online"}), 200
 
     return app
