25b17a8fa68a90f3b1610cd42921a9cc4c468f05
Raquel <raquel.moreirarg23@gmail.com>
2026-04-08 11:47:00 -0300

    Change home route to use @app.route decorator

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ from app import create_app
 
 app = create_app()
 
-@app.get("/teste")
+@app.route("/")
+def home():
+    return {"msg": "API rodando"}
+
+@app.route("/teste")
 def teste():
     return {"msg": "FUNCIONOU"}
-    
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
